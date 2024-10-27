7282fdb5ec22f40e2c7b3d1a9e37bc5f78b5ed6c
berggren00 <andreas_berggren@hotmail.com>
2024-10-27 23:00:28 +0100

    Changed language to english

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ const App = () => {
   const [counter, setCounter] = useState(0);
 
   const INCREMENT_AMOUNT = 45;
-  const RESET_AMOUNT = 0;
   
   function increment() {
     setCounter(counter +1);
@@ -21,17 +20,17 @@ const App = () => {
   }
   
   function resetAmount() {
-    setCounter(RESET_AMOUNT);
+    setCounter(0);
   }
 
   return (
     <div>
       <h1 className="counter">{counter}</h1>
       <h2 className="buttonContainer">
-      <button className="button" onClick={increment}>Öka räknaren</button>
-      <button className="button" onClick={decrement}>Minska räknaren</button>
-      <button className="button" onClick={incrementByAmount}>Öka räknaren med {INCREMENT_AMOUNT}</button>
-      <button className="button" onClick={resetAmount}>Resetta Knappen</button>
+      <button className="button" onClick={increment}>Increase Counter</button>
+      <button className="button" onClick={decrement}>Decrease counter</button>
+      <button className="button" onClick={incrementByAmount}>Increase counter by {INCREMENT_AMOUNT}</button>
+      <button className="button" onClick={resetAmount}>Reset counter</button>
       <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="button" target="_blank" rel="noopener noreferrer">Generate random number</a>
       </h2>
     </div>
