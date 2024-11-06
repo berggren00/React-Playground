85370f0666f50ea6240ad206edd83b6d110e2219
berggren00 <145928987+berggren00@users.noreply.github.com>
2024-11-06 21:00:09 +0100

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,13 +1,5 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-
-
-// Exakt så. Vi hade kunnat skriva showPassword == true ? "text" : "password"
-// men med booleans, så kan du bara skriva "showPassword" för att kolla om den e true
-// precis samma i en "if"
-// if(showpassword) är samma sak som if(showPassword == true)
-
- // ta inte bort alla kommentarer där nere  nu spammas de i konsolen "nu körs" ohhfan. Ahh man ska inte anropa metoder så egentligen. kom ner
   
 
 const Login = () => {
@@ -22,7 +14,7 @@ const Login = () => {
         event.preventDefault();
         
         if(password.length < 5) {
-            alert("Your password is too short. Like my dick.")
+            alert("Your password is too short.")
         } else if(password.trim() && username.trim()) {
             navigate("/app");
         } else {
@@ -30,17 +22,14 @@ const Login = () => {
         }
         
     }
-    // Kolla först längden på password. Kolla sedan username.trim() && password.trim(). 
-
-    // ja kan inte syntax på språken kan man använda minLength eller .length? hemsidan fungerar inte
     
     function togglePassword() {
-        if (showPassword == false) { // false = vi visar INTE password
-            setShowPassword(true) // kommer vi hit så visar vi password
+        if (showPassword == false) {
+            setShowPassword(true) 
         } else if (showPassword == true) {
             setShowPassword(false)
         }
-        // det vi vill göra nu är att toggla type=password beroende på vad showPassword värdet är (true/false)
+       
     }
         
   return (
@@ -61,7 +50,7 @@ const Login = () => {
                 <input
                 required="password"
                 className="input"
-                type={showPassword ? "text" : "password"} // if it works it worksssssss
+                type={showPassword ? "text" : "password"}
                 // Password måste vara minst 5 tecken lång för att vi ska kunna navigeras till /app
                 value={password} //  vadå
                 onChange={(event) => setPassword(event.target.value)} 
